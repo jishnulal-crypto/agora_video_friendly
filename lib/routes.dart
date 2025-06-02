f54f3ee5565b2ccd0a8e37a289a42216d6a1d945
jishnulal-crypto <outpersonel@gmail.com>
2025-06-02 21:24:36 +0530

    routes class initialized

---
+++ lib/routes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
