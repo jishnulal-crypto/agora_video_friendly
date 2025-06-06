e2a8cb9e9e255ef7e6eafc6a70d7c6586788540c
jishnulal-crypto <outpersonel@gmail.com>
2025-06-06 16:06:01 +0530

    firestore call push , send fcm , agora initialization and joinchannel

--- lib/providers/call_provider.dart
+++ lib/providers/call_provider.dart
@@ -1,9 +1,21 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
 import '../services/call_initiation_service.dart';
+import '../services/audio_call_service.dart'; // Agora logic
+
+
 
 class CallProvider extends ChangeNotifier {
   final CallInitiationService _callService = CallInitiationService();
+  final AudioCallService _audioService = AudioCallService();
 
+  // Public getters
+  CallInitiationService get callService => _callService;
+  AudioCallService get audioService => _audioService;
+  RtcEngine get engine => audioService.engine;
+  
+  int? remoteUid;
+  
   bool _isCalling = false;
   bool get isCalling => _isCalling;
 
@@ -15,8 +27,8 @@ class CallProvider extends ChangeNotifier {
     required bool isVideo,
   }) async {
     _isCalling = true;
-    notifyListeners();
 
+    // 1. Start call in Firestore
     await _callService.startCall(
       callerId: callerId,
       receiverId: receiverId,
@@ -24,10 +36,30 @@ class CallProvider extends ChangeNotifier {
       receiverPhoto: receiverPhoto,
       isVideo: isVideo,
     );
+
+    // 2. Start Agora call
+    await _audioService.initialize(
+      type: isVideo,
+      onUserJoined: (uid) {
+        print("Remote user joined: $uid");
+        remoteUid = uid;
+
+      },
+      onUserOffline: (uid) {
+        remoteUid = uid;
+        print("Remote user left: $uid");
+      },
+    );
+    await _audioService.startCall();
   }
 
   Future<void> endCall(String callerId, String receiverId) async {
+    // 1. End Agora session
+    await _audioService.endCall();
+
+    // 2. Update Firestore
     await _callService.endCall(callerId, receiverId);
+
     _isCalling = false;
     notifyListeners();
   }
